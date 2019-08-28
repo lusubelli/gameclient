1e171f0130fb06b181a904311d9dffea87d965f3
usubelli <lionel.usubelli@gmail.com>
2019-08-28 08:16:47 +0200

    toto

--- src/gui/GameRenderer.js
+++ src/gui/GameRenderer.js
@@ -10,8 +10,7 @@ export default class GameRenderer extends React.Component {
         super(props);
 
         let textures = {
-            "world-0": require('../../resources/world-sprite/world/world.png'),
-            "world-1": require('../../resources/world-sprite/world/world.png'),
+            "world": require('../../resources/world-sprite/world/world.png'),
             "mage-sw-walking-0": require('../../resources/world-sprite/mage/sw/walking/0.png'),
             "mage-sw-walking-1": require('../../resources/world-sprite/mage/sw/walking/1.png'),
             "mage-se-walking-0": require('../../resources/world-sprite/mage/se/walking/0.png'),
@@ -30,15 +29,45 @@ export default class GameRenderer extends React.Component {
                 network.onmessage((message) => {
                     if (message.action === "game-updated") {
                         let newGame = JSON.parse(message.payload);
-                        console.log(newGame);
+
+                        let world = {
+                            sprites: []
+                        };
+
+                        world.sprites[world.sprites.length] = {
+                            texture: textures["world"],
+                            coords: { x: 0, y: 0, w: newGame.world.width, h: newGame.world.height}
+                        };
+
+                        newGame.world.buildings.forEach(building => {
+                            world.sprites[world.sprites.length] = {
+                                texture: textures["mage-sw-walking-0"],
+                                coords: { x: building.x, y: building.y, w: building.width, h: building.height}
+                            }
+                        });
+
+                        newGame.world.units.forEach(unit => {
+                            index++;
+                            if(index === 2) {
+                                index = 0;
+                            }
+                            let direction = this.direction(unit.direction);
+                            let action = unit.walking === true ? "walking" : "any";
+                            world.sprites[world.sprites.length] = {
+                                texture: textures[unit.type + "-" + direction + "-" + action + "-" + index],
+                                coords: { x: unit.x, y: unit.y, w: unit.width, h: unit.height}
+                            }
+                        });
+                        console.log(world);
+                        /*
                         newGame.world.sprites.forEach(s => {
                             s.texture = textures[s.texture + "-" + index]
                             index++;
                             if(index === 2) {
                                 index = 0;
                             }
-                        });
-                        this.setState({game: newGame})
+                        });*/
+                        this.setState({game: { world: world}})
                     }
                 });
             });
@@ -49,19 +78,23 @@ export default class GameRenderer extends React.Component {
                 }
             }
         };
-        this.shakingAnimation = new Animated.ValueXY({x: 0, y: 0})
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.game.world.shaking !== this.state.game.world.shaking) {
-            console.log("shaking");
-            this.shake()
+    direction(direction) {
+        if (direction >= 0 && direction <= 90) {
+            return "se";
+        } else if (direction > 90 && direction <= 180) {
+            return "sw";
+        } else if (direction > 180 && direction <= 270) {
+            return "nw";
+        } else {
+            return "ne";
         }
     }
 
     render() {
         return (
-            <Animated.View style={[ this.shakingAnimation.getLayout()]}>
+            <Animated.View>
                 {this.state.game.world.sprites.map((sprite, index) => {
                     return (<Sprite key={index} texture={sprite.texture} coords={sprite.coords}/>)
                 })}
@@ -69,50 +102,4 @@ export default class GameRenderer extends React.Component {
         )
     }
 
-
-
-    shake() {
-        /*
-        Animated.loop(
-            // Animation consists of a sequence of steps
-            Animated.sequence([
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 0},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 10},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 0, y: 10},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 0},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 10},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 0},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 0, y: 10},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 10, y: 0},
-                    duration: 150
-                }),
-                Animated.spring(this.shakingAnimation, {
-                    toValue: {x: 0, y: 0},
-                    duration: 150
-                })
-            ])
-        ).start();*/
-    }
 }
